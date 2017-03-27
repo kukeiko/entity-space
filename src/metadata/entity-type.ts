66088637d6603598b832860c6697e86fc974af41
Magna Marek <magna.marek@gmail.com>
2017-03-27 22:09:05 +0200

    mod: removed "constructor" field @ IEntity

--- src/metadata/entity-type.ts
+++ src/metadata/entity-type.ts
@@ -1,8 +1,6 @@
 import { ITypeOf, IStringIndexable } from "../util";
 
-export interface IEntity extends IStringIndexable {
-    constructor: IEntityType<IEntity>;
-}
+export interface IEntity extends IStringIndexable { }
 
 // todo: rename to IEntityClass
 export interface IEntityType<T extends IEntity> extends ITypeOf<T> { }
