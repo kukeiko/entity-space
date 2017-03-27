a50de4b9cec87b525a468804ebb5d6a001c75553
Magna Marek <magna.marek@gmail.com>
2017-03-27 22:42:21 +0200

    meta: add mocha & coverage text summary reports

--- karma.conf.js
+++ karma.conf.js
@@ -3,39 +3,37 @@ var webpack = require("webpack");
 
 module.exports = function (config) {
     config.set({
+        frameworks: ["jasmine"],
+        browsers: ["Chrome"],
+        port: 6400,
         files: ["./test/main.ts"],
-        preprocessors: {
-            "./test/main.ts": ["webpack"],
-            frameworks: ["jasmine"],
-            browsers: ["Chrome"],
-            port: 6400,
-            webpack: {
-                devtool: "inline-source-map",
-                resolve: {
-                    extensions: [".ts", ".tsx", ".js"]
-                },
-                module: {
-                    rules: [
-                        {
-                            test: /\.ts$/,
-                            loader: "awesome-typescript-loader"
-                        },
-                        {
-                            enforce: "post",
-                            test: /\.ts$/,
-                            loader: "istanbul-instrumenter-loader",
-                            include: /src/
-                        }
-                    ]
-                },
-                plugins: [
-                    // allows source inspection @ browser
-                    new webpack.SourceMapDevToolPlugin({ filename: null, test: /\.(ts|js)($|\?)/i })
+        preprocessors: { "./test/main.ts": ["webpack"], },
+        webpack: {
+            devtool: "inline-source-map",
+            resolve: {
+                extensions: [".ts", ".tsx", ".js"]
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.ts$/,
+                        loader: "awesome-typescript-loader"
+                    },
+                    {
+                        enforce: "post",
+                        test: /\.ts$/,
+                        loader: "istanbul-instrumenter-loader",
+                        include: /src/
+                    }
                 ]
             },
+            plugins: [
+                // allows source inspection @ browser
+                new webpack.SourceMapDevToolPlugin({ filename: null, test: /\.(ts|js)($|\?)/i })
+            ]
         },
-        reporters: ["coverage-istanbul"],
-        coverageIstanbulReporter: { reports: ["html"] },
+        reporters: ["mocha", "coverage-istanbul"],
+        coverageIstanbulReporter: { reports: ["text-summary", "html"] },
         webpackMiddleware: { noInfo: true },
         mime: { "text/x-typescript": ["ts", "tsx"] } // so chrome doesn"t refuse execution
     });
