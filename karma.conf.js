dd8e1abb0d09c382646efabfe899d87a1e6a7580
Magna Marek <magna.marek@gmail.com>
2017-03-27 22:35:33 +0200

    meta: added coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,29 +1,42 @@
 var path = require("path");
-var webpackConfig = require("./webpack.config");
-
-webpackConfig.entry = {
-    "unit-tests": "./test/main.ts"
-};
+var webpack = require("webpack");
 
 module.exports = function (config) {
     config.set({
-        port: 6400,
-        webpack: webpackConfig,
-        browsers: ["Chrome"],
-        frameworks: ["jasmine"],
-        files: [
-            "./test/main.ts"
-        ],
+        files: ["./test/main.ts"],
         preprocessors: {
-            "./test/main.ts": ["webpack"]
+            "./test/main.ts": ["webpack"],
+            frameworks: ["jasmine"],
+            browsers: ["Chrome"],
+            port: 6400,
+            webpack: {
+                devtool: "inline-source-map",
+                resolve: {
+                    extensions: [".ts", ".tsx", ".js"]
+                },
+                module: {
+                    rules: [
+                        {
+                            test: /\.ts$/,
+                            loader: "awesome-typescript-loader"
+                        },
+                        {
+                            enforce: "post",
+                            test: /\.ts$/,
+                            loader: "istanbul-instrumenter-loader",
+                            include: /src/
+                        }
+                    ]
+                },
+                plugins: [
+                    // allows source inspection @ browser
+                    new webpack.SourceMapDevToolPlugin({ filename: null, test: /\.(ts|js)($|\?)/i })
+                ]
+            },
         },
-        reporters: ["mocha"],
-        webpackMiddleware: {
-            noInfo: true
-        },
-        // so chrome doesn't refuse execution
-        mime: {
-            'text/x-typescript': ['ts', 'tsx']
-        }
+        reporters: ["coverage-istanbul"],
+        coverageIstanbulReporter: { reports: ["html"] },
+        webpackMiddleware: { noInfo: true },
+        mime: { "text/x-typescript": ["ts", "tsx"] } // so chrome doesn"t refuse execution
     });
 }
