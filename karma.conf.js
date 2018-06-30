d3444e96ca02c2c266f4b7cba1a385ea3269ecc1
Magna Marek <magna.marek@gmail.com>
2018-06-30 04:03:28 +0200

    [meta] chrome headless for single test runs

--- karma.conf.js
+++ karma.conf.js
@@ -1,21 +1,29 @@
 var path = require("path");
 var webpack = require("webpack");
+let npmCommand = process.env.npm_lifecycle_event;
+
+let browsers = ["ChromeHeadless"];
+
+if (npmCommand.includes(":watch")) {
+    browsers = ["Chrome"];
+}
 
 module.exports = function (config) {
     config.set({
         frameworks: ["jasmine"],
-        browsers: ["Chrome"],
+        browsers: browsers,
         port: 6400,
         files: ["./test/entry.ts"],
-        preprocessors: { "./test/entry.ts": ["webpack"], },
+        preprocessors: {
+            "./test/entry.ts": ["webpack"],
+        },
         webpack: {
             devtool: "inline-source-map",
             resolve: {
                 extensions: [".ts", ".tsx", ".js"]
             },
             module: {
-                rules: [
-                    {
+                rules: [{
                         test: /\.ts$/,
                         loader: "awesome-typescript-loader"
                     },
@@ -30,13 +38,24 @@ module.exports = function (config) {
             },
             plugins: [
                 // allows source inspection @ browser
-                new webpack.SourceMapDevToolPlugin({ filename: null, test: /\.(ts|js)($|\?)/i })
+                new webpack.SourceMapDevToolPlugin({
+                    filename: null,
+                    test: /\.(ts|js)($|\?)/i
+                })
             ]
         },
         reporters: ["mocha", "coverage-istanbul"],
-        mochaReporter: { ignoreSkipped: true },
-        coverageIstanbulReporter: { reports: ["text-summary", "html"] },
-        webpackMiddleware: { noInfo: true },
-        mime: { "text/x-typescript": ["ts", "tsx"] } // so chrome doesn"t refuse execution
+        mochaReporter: {
+            ignoreSkipped: true
+        },
+        coverageIstanbulReporter: {
+            reports: ["text-summary", "html"]
+        },
+        webpackMiddleware: {
+            noInfo: true
+        },
+        mime: {
+            "text/x-typescript": ["ts", "tsx"]
+        } // so chrome doesn"t refuse execution
     });
 }
