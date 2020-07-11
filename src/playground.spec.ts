f1783cb4add06d5d93fc8a2de06be4a23dc370fb
Magna Marek <magna.marek@pbs.one>
2020-07-11 03:03:26 +0200

    WIP: illustrated & cleaned up query prototyping playground a bit

--- src/playground.spec.ts
+++ src/playground.spec.ts
@@ -4,32 +4,51 @@ import { ObjectCriteria } from "./criteria";
 import { Entity } from "./entity";
 import { Selection } from "./selection";
 
-export class TreeNode extends Entity<TreeNode, typeof TreeNode> {
-    static getMetadata(): EntityTypeMetadata<TreeNode> {
-        return {} as any;
-    }
-
-    id: number = 0;
-    name?: string;
-    children?: TreeNode[];
-    parent?: TreeNode | null;
-    parents?: TreeNode[];
-}
-
 /**
  * this is my little playground i use for prototyping.
  */
 describe("prototyping-playground", () => {
-    it("query playground (interfaces)", () => {
-        type PickableSelection<T> = { [K in keyof Selection<T>]-?: K };
+    /**
+     * Our custom user data type.
+     */
+    class TreeNode extends Entity<TreeNode, typeof TreeNode> {
+        static getMetadata(): EntityTypeMetadata<TreeNode> {
+            return {} as any;
+        }
 
+        id: number = 0;
+        name?: string;
+        children?: TreeNode[];
+        parent?: TreeNode | null;
+        parents?: TreeNode[];
+    }
+
+    it("query playground", () => {
+        /**
+         * Our dynamic selection object needs to store the already selected shape somehow,
+         * and we're using a symbol to prevent naming collision (so that users are not restricted in the naming of their data properties).
+         */
         const Selected = Symbol();
 
+        /**
+         * Type required to generate a dynamic type where each selectable property on a type T is expressed
+         * as a function that can be called & chained. See a bit below, there is an actual example using TreeNode.
+         */
         type Selector<T, M = {}> = {
             [K in keyof PickableSelection<T>]: <O extends Selector<T[K]>>(
+                /**
+                 * With expand we can select properties of a nested type like references & children.
+                 */
                 expand?: (selector: Selector<Exclude<Unbox<T[K]>, null | undefined>>) => O
             ) => Selector<T, M & Record<K, O extends undefined ? true : {} extends O[typeof Selected] ? true : O[typeof Selected]>>;
         } & { [Selected]: M };
+
+        /**
+         * Intermediate helper type required for the Selector type above.
+         * 
+         * [todo] this can probably removed and the Selector type be simplified.
+         */
+        type PickableSelection<T> = { [K in keyof Selection<T>]-?: K };
 
         /**
          * Something that knows how to reduce something else of the same shape.
@@ -88,38 +107,62 @@ describe("prototyping-playground", () => {
             queryClassDefaultScope<T>(model: Class<T>): Extract<M, { model: Model.Object<T>; scope: "default" }>;
         }
 
+        /**
+         * Intermediate class to retrieve a query by defining the scope S.
+         */
         interface ScopedQueries<T extends Query> {
             inScope<S extends T["scope"]>(scope: S): Extract<T, { scope: S }>;
             defaultScope(): Extract<T, { scope: "default" }>;
         }
 
+        /**
+         * An object that we can use to tell our factory which type of data we want to load.
+         */
         const treeNodeLevelModel: Model.Number = {
             format: "int32",
             type: "number",
         };
 
+        /**
+         * An object that we can use to tell our factory which type of data we want to load.
+         */
         const treeNodeModel: Model.Object<TreeNode> = {
             class: () => TreeNode,
             type: "object",
         };
 
+        /**
+         * Creating a factory that is typed to know about our queries.
+         */
         const factory = (({} as any) as Queries).addQuery<TreeNodeQuery>().addQuery<TreeNodeQueryInOtherScope>().addQuery<TreeNodeLevelQuery>();
-        const scopes = factory.getScopes();
 
+        /**
+         * Some testing lines to ensure the factory typing works.
+         */
         factory.query(treeNodeLevelModel).inScope("tree-node-level").scope;
         factory.query(treeNodeModel).inScope("other-scope").scope;
         factory.query(treeNodeModel).defaultScope().scope;
         factory.queryClass(TreeNode).inScope("other-scope").model.class;
         factory.queryClassDefaultScope(TreeNode);
 
+        /**
+         * Actual example of creating a query and selecting properties, then having it strictly typed.
+         */
         const loadSomeTreeNodesQuery = factory
             .query(treeNodeModel)
             .defaultScope()
             .select((x) => x.name().children((x) => x.name()))
             .select((x) => x.parent());
 
+        /**
+         * Taking the type of selection on the query and using Selection.Appy<T, S> (where S is of type Selection)
+         * to get back a type of TreeNode where the selected properties are no longer optional.
+         */
         type StrictlyTypedSelectedTreeNode = Selection.Apply<TreeNode, typeof loadSomeTreeNodesQuery["selection"]>;
 
+        /**
+         * Testing out by writing data that conforms to the strictly typed selection.
+         */
         const loadedTreeNodes: StrictlyTypedSelectedTreeNode[] = [
             {
                 children: [
@@ -135,177 +178,5 @@ describe("prototyping-playground", () => {
                 },
             },
         ];
-
-        /**
-         * older prototyping stuff that can probably be deleted soon
-         */
-        type CreateQueryArgs<Q extends Query> = (Reducible extends Q["arguments"] ? {} : { arguments: Q["arguments"] }) &
-            ("default" extends Q["scope"] ? {} : { scope: Q["scope"] });
-
-        type CreateScalarQueryArgs<Q extends ScalarQuery> = CreateQueryArgs<Q> & {
-            model: Q["model"];
-        };
-
-        /**
-         * The instantiated type of a model of an object query.
-         */
-        type ObjectQueryModelInstanceType<Q extends ObjectQuery> = InstanceType<ReturnType<Q["model"]["class"]>>;
-
-        type CreateObjectQueryArgs<Q extends ObjectQuery> = CreateQueryArgs<Q> & {
-            model: Q["model"]["class"];
-            criteria?: ObjectCriteria<ObjectQueryModelInstanceType<Q>>;
-            selection?: Selection<ObjectQueryModelInstanceType<Q>>;
-        };
-
-        function query<Q extends ScalarQuery<any, any, any>>(args: CreateScalarQueryArgs<Q>): Q;
-        function query<Q extends ObjectQuery<any, any, any>>(args: CreateObjectQueryArgs<Q>): Q;
-        function query(...args: any[]): any {
-            return {};
-        }
-
-        query<TreeNodeQuery>({ model: () => TreeNode, selection: { parent: { name: true } }, criteria: [{ id: [{ op: "in", values: new Set([1]) }] }] });
-        query<TreeNodeLevelQuery>({ model: { type: "number", format: "float" }, scope: "tree-node-level" });
-        query<TreeNodeQuery>({ model: () => TreeNode, selection: { name: true }, criteria: [{ id: [{ op: "==", value: 1 }] }] });
-    });
-
-    /**
-     * i had this before i started w/ interface prototyping, can probably delete soon
-     */
-    it("query playground (classes)", () => {
-        interface Reducible {
-            reduce(other: this): this | null;
-        }
-
-        // interface Query<T extends ValueType, A extends Reducible, S> {
-        // class Query<T extends ValueType, A, S> {
-        class Query<T extends Model, A> {
-            constructor(args: { model: T; arguments: A }) {
-                this.model = args.model;
-                this.arguments = args.arguments;
-            }
-
-            readonly model: T;
-            readonly arguments: A;
-        }
-
-        class ObjectQuery<T, A extends QueryArguments = QueryArguments> extends Query<Model.Object<T>, A> {
-            constructor(args: { class: Class<T>; arguments: A; criteria?: ObjectCriteria<T>; selection?: Selection<T> }) {
-                super({
-                    arguments: args.arguments,
-                    model: { class: () => args.class, type: "object" },
-                });
-            }
-
-            criteria?: ObjectCriteria<T>;
-            selection?: Selection<T>;
-        }
-
-        interface ObjectQueryDefaultCtorArgs<T> {
-            criteria?: ObjectCriteria<T>;
-            selection?: Selection<T>;
-        }
-
-        class TreeNodeQuery extends ObjectQuery<TreeNode> {
-            constructor(args: ObjectQueryDefaultCtorArgs<TreeNode>) {
-                super({
-                    arguments: new QueryArguments(),
-                    class: TreeNode,
-                    criteria: args.criteria,
-                    selection: args.selection,
-                });
-            }
-        }
-
-        class QueryArguments implements Reducible {
-            reduce(other: this): this | null {
-                return other;
-            }
-        }
-
-        class TreeNodeLevelQueryArguments extends QueryArguments {
-            readonly treeNodeIds: number[] = [];
-
-            reduce(other: this): this | null {
-                // [todo] use reduction logic from /criteria folder
-                return other;
-            }
-        }
-
-        // interface TreeNodeLevelQuery extends Query<ValueType.Number, Reducible & { ids: number[] }, "tree-node-level"> {}
-        // // interface TreeNodeQuery extends ObjectQuery<TreeNode, {}, "all"> {}
-
-        // interface Workspace<M = {}> {}
-
-        // const foo2: TreeNodeLevelQuery = {
-        //     arguments: {
-        //         // reduce(other: this) {
-        //         //     return other.reduce(this);
-        //         // },
-        //         ids: [3],
-        //     },
-        // } as any;
-
-        // const foo: TreeNodeQuery = {
-        //     model: {
-        //         class: () => TreeNode,
-        //         type: "object",
-        //     },
-        //     criteria: [
-        //         {
-        //             id: [{ op: "from-to", from: { op: ">", value: "3" }, to: { op: "<", value: 9 } }],
-        //         },
-        //     ],
-        //     arguments: {},
-        //     scope: "all",
-        //     selection: {
-        //         parent: {
-        //             name: true,
-        //         },
-        //     },
-        // };
-
-        // const bar = new (foo.model.class())();
-    });
-
-    /**
-     * this is just my little playground for figuring out how to do strictly typed hydrations.
-     */
-    xit("strictly typed hydrations playground", () => {
-        type PickableSelection<T> = { [K in keyof Selection<T>]-?: K };
-
-        type Selector<T, M = {}> = {
-            [K in keyof PickableSelection<T>]: <O extends Selector<T[K]>>(
-                expand?: (selector: Selector<Exclude<Unbox<T[K]>, null | undefined>>) => O
-            ) => Selector<T, M & Record<K, O extends undefined ? true : {} extends O["selected"] ? true : O["selected"]>>;
-        } & {
-            selected: M;
-        };
-
-        const userNode: Selector<TreeNode> = {} as any;
-        const bar = userNode
-            .name()
-            .parent((x) => x.name())
-            .children((x) => x.parent((x) => x.name()).children()).selected;
-
-        type TypeSafeSelectedTreeNode = Selection.Apply<TreeNode, typeof bar>;
-
-        const treeNode: TypeSafeSelectedTreeNode = {
-            id: 3,
-            children: [
-                {
-                    children: [],
-                    id: 8,
-                    parent: {
-                        id: 3,
-                        name: "bar",
-                    },
-                },
-            ],
-            parent: {
-                id: 2,
-                name: "foo",
-            },
-            name: "bar",
-        };
     });
 });
