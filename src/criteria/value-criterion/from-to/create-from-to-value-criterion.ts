39bd01c1d6a2724a2c6c1b60bb5d3ff8a30e3f37
Magna Marek <magna.marek@pbs.one>
2021-05-18 19:46:31 +0200

    health(criteria): #51 remove unused arg type for create from-to

--- src/criteria/value-criterion/from-to/create-from-to-value-criterion.ts
+++ src/criteria/value-criterion/from-to/create-from-to-value-criterion.ts
@@ -2,12 +2,10 @@ import { FromToValueCriterion } from "./from-to-value-criterion";
 
 export function createFromToValueCriterion<T extends number | string>(
     values: [T | undefined, T | undefined],
-    inclusive: boolean | [boolean] | [boolean, boolean] = true
+    inclusive: boolean | [boolean, boolean] = true
 ): FromToValueCriterion {
     if (typeof inclusive === "boolean") {
         inclusive = [inclusive, inclusive];
-    } else if (inclusive.length === 1) {
-        inclusive = [inclusive[0], true];
     }
 
     const criterion: FromToValueCriterion = { op: "from-to" };
