987ef21dd837bbc51d6570063c992c8c2ba0fb10
Magna Marek <magna.marek@pbs.one>
2021-05-28 20:08:05 +0200

    docs: #49 polished introduction.spec a bit

--- test/introduction/introduction.spec.ts
+++ test/introduction/introduction.spec.ts
@@ -15,6 +15,9 @@ describe("introduction", () => {
          *
          * They then increase the price range to 100 to 300 - and we only want to load the data that is missing, i.e. the products with a price of 200 to 300 euro.
          *
+         * That means that we'll somehow cache the first result, then load the difference, merge it with what is cached, and render it.
+         * However, we're only focusing on the reduction here, so there is no code that does the actual caching / loading.
+         *
          * Our filter (from now on: criteria) for the first call (that is, load products with price 100 - 200) would look like this:
          */
         const initialCriteria = createFromToValueCriterion([100, 200]);
@@ -26,21 +29,13 @@ describe("introduction", () => {
 
         /**
          * We now want to know the difference between those two so we only load the difference from the server.
-         * We can figure out the difference by reducing the secondCriteria by the initialCriteria, that is
+         * We can figure out the difference by reducing the secondCriteria by the initialCriteria, that is:
          * take away from the secondCriteria the intersection it has with the initialCriteria:
          */
         const difference = reduceFromToValueCriterion(secondCriteria, initialCriteria);
 
-        if (!difference) {
-            /**
-             * If the result of the reduction is "false", then no reduction at all was made (i.e. there was no intersection).
-             * In our case, that should never happen. We'll therefore call fail()
-             */
-            fail("reduction did not happen as expected");
-        }
-
         /**
-         * The difference should now be the following criteria: a from-to starting at bigger 200 until less than 300,
+         * The difference should now be the following criteria: a from-to starting at bigger 200 until less than equals 300,
          * which represents the data we still need to load.
          */
         const expected: ValueCriteria = [{ op: "from-to", from: { op: ">", value: 200 }, to: { op: "<=", value: 300 } }];
