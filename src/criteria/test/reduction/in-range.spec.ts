423b8bb6408ec5dbeb9731901ee1279a5750ac76
Magna Marek <magna.marek@pbs.one>
2021-07-17 22:06:56 +0200

    health(criteria): #78 made in-range reduction spec easier to read

--- src/criteria/test/reduction/in-range.spec.ts
+++ src/criteria/test/reduction/in-range.spec.ts
@@ -1,124 +1,51 @@
-import { inRange, inSet, or } from "../../value-criterion";
+import { inSet } from "../../value-criterion";
 import { reducing } from "./reducing.fn";
 
+// [todo] parse errors prevent other specs from running
 describe("reducing: in-range", () => {
     describe("full reduction", () => {
-        reducing(inRange(1, 7)).by(inRange(1, 7)).is(true);
-        reducing(inRange(1, 7)).by(inRange(0, 8)).is(true);
-        reducing(inRange(1, 7)).by(inRange(0, 8, false)).is(true);
-
-        reducing(inRange(1, 7))
-            .by(inRange(0, void 0))
-            .is(true);
-
-        reducing(inRange(4, void 0))
-            .by(inRange(3, void 0))
-            .is(true);
-
-        reducing(inRange(void 0, 4))
-            .by(inRange(void 0, 5))
-            .is(true);
-
-        reducing(inRange(1, 7))
-            .by(inRange(void 0, 9))
-            .is(true);
+        reducing("[1, 7]").by("[1, 7]").is(true);
+        reducing("[1, 7]").by("[0, 8]").is(true);
+        reducing("[1, 7]").by("(0, 8)").is(true);
+        reducing("[1, 7]").by("[0, ...]").is(true);
+        reducing("[4, ...]").by("[3, ...]").is(true);
+        reducing("[..., 4]").by("[..., 5]").is(true);
+        reducing("[1, 7]").by("[..., 9]").is(true);
     });
 
     describe("partial reduction", () => {
         describe("head reduction", () => {
-            reducing(inRange(1, 7))
-                .by(inRange(-3, 5))
-                .is(inRange(5, 7, [false, true]));
-
-            reducing(inRange(3, void 0))
-                .by(inRange(1, 8))
-                .is(inRange(8, void 0, false));
-
-            reducing(inRange(3, void 0))
-                .by(inRange(1, 8, [true, false]))
-                .is(inRange(8, void 0));
-
-            reducing(inRange(1, 7))
-                .by(inRange(void 0, 3, false))
-                .is(inRange(3, 7));
+            reducing("[1, 7]").by("[-3, 5]").is("(5, 7]");
+            reducing("[3, ...]").by("[1, 8]").is("(8, ...]");
+            reducing("[3, ...]").by("[1, 8)").is("[8, ...]");
+            reducing("[1, 7]").by("[..., 3)").is("[3, 7]");
         });
 
         describe("tail reduction", () => {
-            reducing(inRange(1, 7))
-                .by(inRange(3, 10))
-                .is(inRange(1, 3, [true, false]));
-
-            reducing(inRange(1, 7))
-                .by(inRange(3, 8, [false, true]))
-                .is(inRange(1, 3));
-
-            reducing(inRange(void 0, 3))
-                .by(inRange(1, 8))
-                .is(inRange(void 0, 1, false));
-
-            reducing(inRange(void 0, 3))
-                .by(inRange(1, 8, [false, true]))
-                .is(inRange(void 0, 1));
-
-            reducing(inRange(1, 7))
-                .by(inRange(3, void 0))
-                .is(inRange(1, 3, [true, false]));
+            reducing("[1, 7]").by("[3, 10]").is("[1, 3)");
+            reducing("[1, 7]").by("(3, 8]").is("[1, 3]");
+            reducing("[..., 3]").by("[1, 8]").is("[..., 1)");
+            reducing("[..., 3]").by("(1, 8]").is("[..., 1]");
+            reducing("[1, 7]").by("[3, ...]").is("[1, 3)");
         });
 
         describe("body reduction", () => {
-            reducing(inRange(1, 7))
-                .by(inRange(3, 4))
-                .is(or([inRange(1, 3, [true, false]), inRange(4, 7, [false, true])]));
-
-            reducing(inRange(1, 7, false))
-                .by(inRange(3, 4))
-                .is(or([inRange(1, 3, false), inRange(4, 7, false)]));
-
-            reducing(inRange(1, 7, false))
-                .by(inRange(3, 3, false))
-                .is(or([inRange(1, 3, [false, true]), inRange(3, 7, [true, false])]));
-
-            reducing(inRange(void 0, 7))
-                .by(inRange(3, 4))
-                .is(or([inRange(void 0, 3, false), inRange(4, 7, [false, true])]));
-
-            reducing(inRange(void 0, 7))
-                .by(inRange(3, 4, false))
-                .is(or([inRange(void 0, 3), inRange(4, 7)]));
-
-            reducing(inRange(1, void 0))
-                .by(inRange(3, 4))
-                .is(or([inRange(1, 3, [true, false]), inRange(4, void 0, false)]));
-
-            reducing(inRange(1, void 0))
-                .by(inRange(3, 4, false))
-                .is(or([inRange(1, 3), inRange(4, void 0)]));
-
-            reducing(inRange(1, 7))
-                .by(inRange(1, 7, false))
-                .is(or([inRange(1, 1), inRange(7, 7)]));
+            reducing("[1, 7]").by("[3, 4]").is("[1, 3) | (4, 7]");
+            reducing("(1, 7)").by("[3, 4]").is("(1, 3) | (4, 7)");
+            reducing("(1, 7)").by("(3, 3)").is("(1, 3] | [3, 7)");
+            reducing("[..., 7]").by("[3, 4]").is("[..., 3) | (4, 7]");
+            reducing("[..., 7]").by("(3, 4)").is("[..., 3] | [4, 7]");
+            reducing("[1, ...]").by("[3, 4]").is("[1, 3) | (4, ...]");
+            reducing("[1, ...]").by("(3, 4)").is("[1, 3] | [4, ...]");
+            reducing("[1, 7]").by("(1, 7)").is("[1, 1] | [7, 7]");
         });
 
         describe("reduction by: in", () => {
-            reducing(inRange(1, 2))
-                .by(inSet([2]))
-                .is(inRange(1, 2, [true, false]));
-
-            reducing(inRange(1, 2))
-                .by(inSet([1]))
-                .is(inRange(1, 2, [false, true]));
-
-            reducing(inRange(1, 2))
-                .by(inSet([1, 2]))
-                .is(inRange(1, 2, false));
-
-            reducing(inRange(void 0, 2))
-                .by(inSet([1, 2]))
-                .is(inRange(void 0, 2, false));
-
-            reducing(inRange(1, void 0))
-                .by(inSet([1, 2]))
-                .is(inRange(1, void 0, false));
+            reducing("[1, 2]").by("{2}").is("[1, 2)");
+            reducing("[1, 2]").by("{1}").is("(1, 2]");
+            reducing("[1, 2]").by("{1, 2}").is("(1, 2)");
+            reducing("[..., 2]").by("{1, 2}").is("[..., 2)");
+            reducing("[1, ...]").by("{1, 2}").is("(1, ...]");
         });
     });
 
@@ -126,21 +53,13 @@ describe("reducing: in-range", () => {
         // [todo] now that we allow for splitting a range into smaller parts we might want this reduction to actually do something
         // (instead of doing nothing). the problem is though that a "from-to" reduced by an "in" with lots of values will create
         // lots of queries and hinder performance. also, it would only work with from-to of type integer (which we don't distinguish yet)
-        reducing(inRange(1, 3))
+        reducing("[1, 3]")
             .by(inSet([2]))
             .is(false);
 
-        reducing(inRange(1, 7))
-            .by(inRange(7, 13, [false, true]))
-            .is(false);
-        reducing(inRange(1, 7)).by(inRange(8, 13)).is(false);
-
-        reducing(inRange(1, 7))
-            .by(inRange(void 0, 1, false))
-            .is(false);
-
-        reducing(inRange(1, 7))
-            .by(inRange(void 0, 0))
-            .is(false);
+        reducing("[1, 7]").by("(7, 13]").is(false);
+        reducing("[1, 7]").by("[8, 13]").is(false);
+        reducing("[1, 7]").by("[..., 1)").is(false);
+        reducing("[1, 7]").by("[..., 0]").is(false);
     });
 });
--- src/criteria/test/reduction/reducing.fn.ts
+++ src/criteria/test/reduction/reducing.fn.ts
@@ -1,20 +1,34 @@
-import { ValueCriterion } from "../../value-criterion";
+import { parseCriteria, ValueCriterion } from "../../value-criterion";
+
+function parseIfNecessary<T extends ValueCriterion | string>(item: T): ValueCriterion {
+    if (typeof item === "string") {
+        return parseCriteria(item);
+    }
+
+    return item;
+}
 
 export function reducing(
-    criterion: ValueCriterion
+    criterion: ValueCriterion | string
 ): {
-    by(other: ValueCriterion): { is(expected: ValueCriterion | boolean): void };
+    by(other: ValueCriterion | string): { is(expected: ValueCriterion | string | boolean): void };
 } {
+    const parsedCriterion = parseIfNecessary(criterion);
+
     return {
-        by(other: ValueCriterion) {
+        by(other: ValueCriterion | string) {
+            const parsedOther = parseIfNecessary(other);
+
             return {
-                is(expected: ValueCriterion | boolean) {
+                is(expected: ValueCriterion | string | boolean) {
                     if (expected === true) {
-                        it(`${criterion} should be fully reduced by ${other}`, () => expect(other.reduce(criterion).toString()).toEqual("true"));
+                        it(`${criterion} should be fully reduced by ${other}`, () => expect(parsedOther.reduce(parsedCriterion).toString()).toEqual("true"));
                     } else if (expected === false) {
-                        it(`${criterion} should not be reduced by ${other}`, () => expect(other.reduce(criterion).toString()).toEqual("false"));
+                        it(`${criterion} should not be reduced by ${other}`, () => expect(parsedOther.reduce(parsedCriterion).toString()).toEqual("false"));
                     } else {
-                        it(`${criterion} reduced by ${other} should be ${expected}`, () => expect(other.reduce(criterion).toString()).toEqual(expected.toString()));
+                        const parsedExpected = parseIfNecessary(expected);
+                        it(`${criterion} reduced by ${other} should be ${expected}`, () =>
+                            expect(parsedOther.reduce(parsedCriterion).toString()).toEqual(parsedExpected.toString()));
                     }
                 },
             };
