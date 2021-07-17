fc874deb5580ae6cf157dabe68e68ab79cedb991
Magna Marek <magna.marek@pbs.one>
2021-07-17 19:43:15 +0200

    health(criteria): #78 added concise reduction spec helper

--- test/unit/criteria/criteria-reduction-helpers.ts
+++ test/unit/criteria/criteria-reduction-helpers.ts
@@ -1,15 +1,23 @@
 import { ValueCriterion } from "../../../src";
 
-// using .toString() so we see the string representations of the failed reductions
-
-export function isFullyReduced(what: ValueCriterion, by: ValueCriterion) {
-    it(`${what} should be fully reduced by ${by}`, () => expect(by.reduce(what).toString()).toEqual("true"));
-}
-
-export function isNotReduced(what: ValueCriterion, by: ValueCriterion) {
-    it(`${what} should not be reduced by ${by}`, () => expect(by.reduce(what).toString()).toEqual("false"));
-}
-
-export function isPartiallyReduced(what: ValueCriterion, by: ValueCriterion, expected: ValueCriterion) {
-    it(`${what} reduced by ${by} should be ${expected}`, () => expect(by.reduce(what).toString()).toEqual(expected.toString()));
+export function reducing(
+    criterion: ValueCriterion
+): {
+    by(other: ValueCriterion): { is(expected: ValueCriterion | boolean): void };
+} {
+    return {
+        by(other: ValueCriterion) {
+            return {
+                is(expected: ValueCriterion | boolean) {
+                    if (expected === true) {
+                        it(`${criterion} should be fully reduced by ${other}`, () => expect(other.reduce(criterion).toString()).toEqual("true"));
+                    } else if (expected === false) {
+                        it(`${criterion} should not be reduced by ${other}`, () => expect(other.reduce(criterion).toString()).toEqual("false"));
+                    } else {
+                        it(`${criterion} reduced by ${other} should be ${expected}`, () => expect(other.reduce(criterion).toString()).toEqual(expected.toString()));
+                    }
+                },
+            };
+        },
+    };
 }
--- test/unit/criteria/reduce-in-range.spec.ts
+++ test/unit/criteria/reduce-in-range.spec.ts
@@ -1,50 +1,124 @@
 import { inRange, inSet, or } from "../../../src";
-import { isFullyReduced, isNotReduced, isPartiallyReduced } from "./criteria-reduction-helpers";
+import { reducing } from "./criteria-reduction-helpers";
 
-xdescribe("reduce: in-range", () => {
+describe("reduce: in-range", () => {
     describe("full reduction", () => {
-        isFullyReduced(inRange(1, 7), inRange(1, 7));
-        isFullyReduced(inRange(1, 7), inRange(0, 8));
-        isFullyReduced(inRange(1, 7), inRange(0, 8, false));
-        isFullyReduced(inRange(1, 7), inRange(0, void 0));
-        isFullyReduced(inRange(4, void 0), inRange(3, void 0));
-        isFullyReduced(inRange(void 0, 4), inRange(void 0, 5));
-        isFullyReduced(inRange(1, 7), inRange(void 0, 9));
+        reducing(inRange(1, 7)).by(inRange(1, 7)).is(true);
+        reducing(inRange(1, 7)).by(inRange(0, 8)).is(true);
+        reducing(inRange(1, 7)).by(inRange(0, 8, false)).is(true);
+
+        reducing(inRange(1, 7))
+            .by(inRange(0, void 0))
+            .is(true);
+
+        reducing(inRange(4, void 0))
+            .by(inRange(3, void 0))
+            .is(true);
+
+        reducing(inRange(void 0, 4))
+            .by(inRange(void 0, 5))
+            .is(true);
+
+        reducing(inRange(1, 7))
+            .by(inRange(void 0, 9))
+            .is(true);
     });
 
     describe("partial reduction", () => {
         describe("head reduction", () => {
-            isPartiallyReduced(inRange(1, 7), inRange(-3, 5), inRange(5, 7, [false, true]));
-            isPartiallyReduced(inRange(3, void 0), inRange(1, 8), inRange(8, void 0, false));
-            isPartiallyReduced(inRange(3, void 0), inRange(1, 8, [true, false]), inRange(8, void 0));
-            isPartiallyReduced(inRange(1, 7), inRange(void 0, 3, false), inRange(3, 7));
+            reducing(inRange(1, 7))
+                .by(inRange(-3, 5))
+                .is(inRange(5, 7, [false, true]));
+
+            reducing(inRange(3, void 0))
+                .by(inRange(1, 8))
+                .is(inRange(8, void 0, false));
+
+            reducing(inRange(3, void 0))
+                .by(inRange(1, 8, [true, false]))
+                .is(inRange(8, void 0));
+
+            reducing(inRange(1, 7))
+                .by(inRange(void 0, 3, false))
+                .is(inRange(3, 7));
         });
 
-        fdescribe("tail reduction", () => {
-            isPartiallyReduced(inRange(1, 7), inRange(3, 10), inRange(1, 3, [true, false]));
-            isPartiallyReduced(inRange(1, 7), inRange(3, 8, [false, true]), inRange(1, 3));
-            isPartiallyReduced(inRange(void 0, 3), inRange(1, 8), inRange(void 0, 1, false));
-            isPartiallyReduced(inRange(void 0, 3), inRange(1, 8, [false, true]), inRange(void 0, 1));
-            isPartiallyReduced(inRange(1, 7), inRange(3, void 0), inRange(1, 3, [true, false]));
+        describe("tail reduction", () => {
+            reducing(inRange(1, 7))
+                .by(inRange(3, 10))
+                .is(inRange(1, 3, [true, false]));
+
+            reducing(inRange(1, 7))
+                .by(inRange(3, 8, [false, true]))
+                .is(inRange(1, 3));
+
+            reducing(inRange(void 0, 3))
+                .by(inRange(1, 8))
+                .is(inRange(void 0, 1, false));
+
+            reducing(inRange(void 0, 3))
+                .by(inRange(1, 8, [false, true]))
+                .is(inRange(void 0, 1));
+
+            reducing(inRange(1, 7))
+                .by(inRange(3, void 0))
+                .is(inRange(1, 3, [true, false]));
         });
 
         describe("body reduction", () => {
-            isPartiallyReduced(inRange(1, 7), inRange(3, 4), or([inRange(1, 3, [true, false]), inRange(4, 7, [false, true])]));
-            isPartiallyReduced(inRange(1, 7, false), inRange(3, 4), or([inRange(1, 3, false), inRange(4, 7, false)]));
-            isPartiallyReduced(inRange(1, 7, false), inRange(3, 3, false), or([inRange(1, 3, [false, true]), inRange(3, 7, [true, false])]));
-            isPartiallyReduced(inRange(void 0, 7), inRange(3, 4), or([inRange(void 0, 3, false), inRange(4, 7, [false, true])]));
-            isPartiallyReduced(inRange(void 0, 7), inRange(3, 4, false), or([inRange(void 0, 3), inRange(4, 7)]));
-            isPartiallyReduced(inRange(1, void 0), inRange(3, 4), or([inRange(1, 3, [true, false]), inRange(4, void 0, false)]));
-            isPartiallyReduced(inRange(1, void 0), inRange(3, 4, false), or([inRange(1, 3), inRange(4, void 0)]));
-            isPartiallyReduced(inRange(1, 7), inRange(1, 7, false), or([inRange(1, 1), inRange(7, 7)]));
+            reducing(inRange(1, 7))
+                .by(inRange(3, 4))
+                .is(or([inRange(1, 3, [true, false]), inRange(4, 7, [false, true])]));
+
+            reducing(inRange(1, 7, false))
+                .by(inRange(3, 4))
+                .is(or([inRange(1, 3, false), inRange(4, 7, false)]));
+
+            reducing(inRange(1, 7, false))
+                .by(inRange(3, 3, false))
+                .is(or([inRange(1, 3, [false, true]), inRange(3, 7, [true, false])]));
+
+            reducing(inRange(void 0, 7))
+                .by(inRange(3, 4))
+                .is(or([inRange(void 0, 3, false), inRange(4, 7, [false, true])]));
+
+            reducing(inRange(void 0, 7))
+                .by(inRange(3, 4, false))
+                .is(or([inRange(void 0, 3), inRange(4, 7)]));
+
+            reducing(inRange(1, void 0))
+                .by(inRange(3, 4))
+                .is(or([inRange(1, 3, [true, false]), inRange(4, void 0, false)]));
+
+            reducing(inRange(1, void 0))
+                .by(inRange(3, 4, false))
+                .is(or([inRange(1, 3), inRange(4, void 0)]));
+
+            reducing(inRange(1, 7))
+                .by(inRange(1, 7, false))
+                .is(or([inRange(1, 1), inRange(7, 7)]));
         });
 
         describe("reduction by: in", () => {
-            isPartiallyReduced(inRange(1, 2), inSet([2]), inRange(1, 2, [true, false]));
-            isPartiallyReduced(inRange(1, 2), inSet([1]), inRange(1, 2, [false, true]));
-            isPartiallyReduced(inRange(1, 2), inSet([1, 2]), inRange(1, 2, false));
-            isPartiallyReduced(inRange(void 0, 2), inSet([1, 2]), inRange(void 0, 2, false));
-            isPartiallyReduced(inRange(1, void 0), inSet([1, 2]), inRange(1, void 0, false));
+            reducing(inRange(1, 2))
+                .by(inSet([2]))
+                .is(inRange(1, 2, [true, false]));
+
+            reducing(inRange(1, 2))
+                .by(inSet([1]))
+                .is(inRange(1, 2, [false, true]));
+
+            reducing(inRange(1, 2))
+                .by(inSet([1, 2]))
+                .is(inRange(1, 2, false));
+
+            reducing(inRange(void 0, 2))
+                .by(inSet([1, 2]))
+                .is(inRange(void 0, 2, false));
+
+            reducing(inRange(1, void 0))
+                .by(inSet([1, 2]))
+                .is(inRange(1, void 0, false));
         });
     });
 
@@ -52,10 +126,21 @@ xdescribe("reduce: in-range", () => {
         // [todo] now that we allow for splitting a range into smaller parts we might want this reduction to actually do something
         // (instead of doing nothing). the problem is though that a "from-to" reduced by an "in" with lots of values will create
         // lots of queries and hinder performance. also, it would only work with from-to of type integer (which we don't distinguish yet)
-        isNotReduced(inRange(1, 3), inSet([2]));
-        isNotReduced(inRange(1, 7), inRange(7, 13, [false, true]));
-        isNotReduced(inRange(1, 7), inRange(8, 13));
-        isNotReduced(inRange(1, 7), inRange(void 0, 1, false));
-        isNotReduced(inRange(1, 7), inRange(void 0, 0));
+        reducing(inRange(1, 3))
+            .by(inSet([2]))
+            .is(false);
+
+        reducing(inRange(1, 7))
+            .by(inRange(7, 13, [false, true]))
+            .is(false);
+        reducing(inRange(1, 7)).by(inRange(8, 13)).is(false);
+
+        reducing(inRange(1, 7))
+            .by(inRange(void 0, 1, false))
+            .is(false);
+
+        reducing(inRange(1, 7))
+            .by(inRange(void 0, 0))
+            .is(false);
     });
 });
