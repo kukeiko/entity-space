d013a42b630a4a6e381bfe2fea81d3c8bff31b04
Magna Marek <magna.marek@pbs.one>
2021-07-17 22:34:09 +0200

    health(criteria): #78 made in-set / not-in-set reduction specs more concise

--- src/criteria/test/reduction/in-set.spec.ts
+++ src/criteria/test/reduction/in-set.spec.ts
@@ -1,116 +1,29 @@
-import { inSet, notInSet, inRange } from "../../value-criterion";
+import { reducing } from "./reducing.fn";
 
 describe("reducing: in-set", () => {
     /**
      * A criteria that is a subset of another should always be completely reduced.
      */
     describe("full reduction", () => {
-        it("{1, 2, 3} should be completely reduced by itself", () => {
-            // arrange
-            const a = inSet([1, 2, 3]);
-            const b = inSet([1, 2, 3]);
-
-            // act
-            const reduced = b.reduce(a);
-
-            // assert
-            expect(reduced).toEqual(true);
-        });
-
-        it("{1, 2, 3} should be completely reduced by {1, 2, 3, 4}", () => {
-            // arrange
-            const a = inSet([1, 2, 3]);
-            const b = inSet([1, 2, 3, 4]);
-
-            // act
-            const reduced = b.reduce(a);
-
-            // assert
-            expect(reduced).toEqual(true);
-        });
-
-        it("{1, 2, 3} should be completely reduced by !{4}", () => {
-            // arrange
-            const a = inSet([1, 2, 3]);
-            const b = notInSet([4]);
-
-            // act
-            const reduced = b.reduce(a);
-
-            // assert
-            expect(reduced).toEqual(true);
-        });
+        reducing("{1, 2, 3}").by("{1, 2, 3}").is(true);
+        reducing("{1, 2, 3}").by("{1, 2, 3, 4}").is(true);
+        reducing("{1, 2, 3}").by("!{4}").is(true);
     });
 
     /**
      * An intersection between two criteria should always be removed.
      */
     describe("partial reduction", () => {
-        it("{1, 2, 3} reduced by {1, 2, 4} should be {3}", () => {
-            // arrange
-            const a = inSet([1, 2, 3]);
-            const b = inSet([1, 2, 4]);
-            const expected = inSet([3]);
-
-            // act
-            const reduced = b.reduce(a);
-
-            // assert
-            expect(reduced).toEqual(expected);
-        });
-
-        it("{1, 2, 3} reduced by !{1} should be {1}", () => {
-            // arrange
-            const a = inSet([1, 2, 3]);
-            const b = notInSet([1]);
-            const expected = inSet([1]);
-
-            // act
-            const reduced = b.reduce(a);
-
-            // assert
-            expect(reduced).toEqual(expected);
-        });
+        reducing("{1, 2, 3}").by("{1, 2, 4}").is("{3}");
+        reducing("{1, 2, 3}").by("!{1}").is("{1}");
     });
 
     /**
      * If there is no intersection, the criteria should be left untouched.
      */
     describe("no reduction", () => {
-        it("{1, 2, 3} should not be reduced by {4, 5, 6}", () => {
-            // arrange
-            const a = inSet([1, 2, 3]);
-            const b = inSet([4, 5, 6]);
-
-            // act
-            const reduced = b.reduce(a);
-
-            // assert
-            expect(reduced).toBeFalse();
-        });
-
-        it("{1, 2, 3} should not be reduced by !{1, 2, 3}", () => {
-            // arrange
-            const a = inSet([1, 2, 3]);
-            const b = notInSet([1, 2, 3]);
-
-            // act
-            const reduced = b.reduce(a);
-
-            // assert
-            expect(reduced).toBeFalse();
-        });
-
-        it("{1, 2, 3} should not be reduced by [1, 3]", () => {
-            // arrange
-            const a = inSet([1, 2, 3]);
-            const b = inRange(4, 7);
-
-            // act
-            const reduced = b.reduce(a);
-
-            // assert
-            expect(reduced).toBeFalse();
-        });
+        reducing("{1, 2, 3}").by("{4, 5, 6}").is(false);
+        reducing("{1, 2, 3}").by("!{1, 2, 3}").is(false);
+        reducing("{1, 2, 3}").by("[1, 3]").is(false);
     });
 });
