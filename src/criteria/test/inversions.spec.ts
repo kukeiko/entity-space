f852b543a98b90dce532b2100e1f135c1af1eeb1
Magna Marek <magna.marek@pbs.one>
2021-07-31 22:20:01 +0200

    test(criteria): made inversion specs more concise by using string parsing

--- src/criteria/test/inversions.spec.ts
+++ src/criteria/test/inversions.spec.ts
@@ -1,105 +1,19 @@
-import { inRange, inSet, isEven, notInSet, or } from "../criterion";
+import { inverting } from "./inverting.fn";
 
-describe("invert: binary", () => {
-    it("is-even inverted should be is-odd", () => {
-        // arrange
-        const criterion = isEven(true);
-        const expected = isEven(false);
+describe("inverting criteria", () => {
+    // binary
+    inverting("is-even").is("is-odd");
 
-        // act
-        const inverted = criterion.invert();
+    // in-set
+    inverting("{1, 2, 3}").is("!{1, 2, 3}");
 
-        // assert
-        expect(inverted).toEqual(expected);
-    });
-});
+    // not-in-set
+    inverting("!{1, 2, 3}").is("{1, 2, 3}");
 
-describe("invert: in-set", () => {
-    it("{1, 2, 3} inverted should be !{1, 2, 3}", () => {
-        // arrange
-        const criterion = inSet([1, 2, 3]);
-        const expected = notInSet([1, 2, 3]);
-
-        // act
-        const actual = criterion.invert();
-
-        // assert
-        expect(actual).toEqual(expected);
-    });
-});
-
-describe("invert: not-in-set", () => {
-    it("!{1, 2, 3} inverted should be {1, 2, 3}", () => {
-        // arrange
-        const criterion = notInSet([1, 2, 3]);
-        const expected = inSet([1, 2, 3]);
-
-        // act
-        const actual = criterion.invert();
-
-        // assert
-        expect(actual).toEqual(expected);
-    });
-});
-
-describe("invert: in-range", () => {
-    it("[1, 7] inverted should be [..., 1) | (7, ...]", () => {
-        // arrange
-        const criterion = inRange(1, 7);
-        const expected = or([inRange(void 0, 1, false), inRange(7, void 0, false)]);
-
-        // act
-        const actual = criterion.invert();
-
-        // assert
-        expect(actual).toEqual(expected);
-    });
-
-    it("(1, 7] inverted should be [..., 1] | (7, ...]", () => {
-        // arrange
-        const criterion = inRange(1, 7, [false, true]);
-        const expected = or([inRange(void 0, 1), inRange(7, void 0, false)]);
-
-        // act
-        const actual = criterion.invert();
-
-        // assert
-        expect(actual).toEqual(expected);
-    });
-
-    it("(1, 7) inverted should be [..., 1] | [7, ...]", () => {
-        // arrange
-        const criterion = inRange(1, 7, false);
-        const expected = or([inRange(void 0, 1), inRange(7, void 0)]);
-
-        // act
-        const actual = criterion.invert();
-
-        // assert
-        expect(actual).toEqual(expected);
-    });
-
-    it("[..., 7] inverted should be (7, ...]", () => {
-        // arrange
-        const criterion = inRange(void 0, 7);
-        const expected = inRange(7, void 0, false);
-
-        // act
-        const actual = criterion.invert();
-
-        // assert
-        expect(actual).toEqual(expected);
-    });
-
-    it("[7, ...] inverted should be [..., 7)", () => {
-        // arrange
-        const criterion = inRange(7, void 0);
-        const expected = inRange(void 0, 7, false);
-
-        // act
-        const actual = criterion.invert();
-
-        // assert
-        expect(actual).toEqual(expected);
-    });
+    // in-range
+    inverting("[1, 7]").is("[..., 1) | (7, ...]");
+    inverting("(1, 7]").is("[..., 1] | (7, ...]");
+    inverting("(1, 7)").is("[..., 1] | [7, ...]");
+    inverting("[..., 7]").is("(7, ...]");
+    inverting("[7, ...]").is("[..., 7)");
 });
