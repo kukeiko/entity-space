7125b912952e2b3a207952f03a0ba58d40d985f5
Magna Marek <magna.marek@gmail.com>
2021-12-28 19:10:24 +0100

    feat: #115 generated sandbox-ui

---
+++ packages/apps/sandbox-ui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "entity-space-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.scss"],
+})
+export class AppComponent {
+    title = "apps-sandbox-ui";
+}
