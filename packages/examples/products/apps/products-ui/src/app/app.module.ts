13ba48c99daa961abb17ec638ed760e243b3e797
Magna Marek <magna.marek@gmail.com>
2022-01-16 18:51:01 +0100

    feat: #114 added @angular/material to products example

--- packages/examples/products/apps/products-ui/src/app/app.module.ts
+++ packages/examples/products/apps/products-ui/src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppComponent } from "./app.component";
 import { ProductEntitySource } from "./entity-sources/product.entity-source";
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, HttpClientModule, FormsModule],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+    ],
     providers: [ProductEntitySource],
     bootstrap: [AppComponent],
 })
