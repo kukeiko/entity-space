f791e872ba9308132b7f4bf2f3f463a722c84d75
Magna Marek <magna.marek@gmail.com>
2022-01-22 15:40:33 +0100

    health: #152 generated lexer package using "nx generate @nrwl/js:library --name=lexer --buildable"

---
+++ packages/lexer/src/lib/lexer.spec.ts
@@ -0,0 +1,7 @@
+import { lexer } from "./lexer";
+
+describe("lexer", () => {
+    it("should work", () => {
+        expect(lexer()).toEqual("lexer");
+    });
+});
---
+++ packages/lexer/src/lib/lexer.ts
@@ -0,0 +1,3 @@
+export function lexer(): string {
+    return "lexer";
+}
