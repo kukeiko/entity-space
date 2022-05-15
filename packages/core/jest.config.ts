ff8e89f4e66e5fc1bde170e11ad69fd46abdc3dc
Magna Marek <magna.marek@gmail.com>
2022-05-15 18:37:21 +0200

    chore: #171 ran nx migrate latest incl. migrations complete wipe of node_modules + package-lock.json was required

--- packages/core/jest.config.ts
+++ packages/core/jest.config.ts
@@ -1,5 +1,6 @@
-module.exports = {
-    displayName: "utils",
+/* eslint-disable */
+export default {
+    displayName: "core",
     preset: "../../jest.preset.js",
     globals: {
         "ts-jest": {
@@ -11,5 +12,5 @@ module.exports = {
         "^.+\\.[tj]sx?$": "ts-jest",
     },
     moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-    coverageDirectory: "../../coverage/packages/utils",
+    coverageDirectory: "../../coverage/packages/core",
 };
