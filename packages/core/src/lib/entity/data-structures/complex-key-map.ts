b313d059c7bdb8f3807f0c35441c5663557a67dd
Magna Marek <magna.marek@gmail.com>
2022-06-21 15:45:13 +0200

    fix: #182 ComplexKeyMap was not using returned value of update() correctly it is now back to the way it was, at some point I changed it from "===" to "!==", and i'm not sure why. tested music-box-app & ui, seem to work fine. reason i found it was a failing createCriterionFromEntities() test

--- packages/core/src/lib/entity/data-structures/complex-key-map.ts
+++ packages/core/src/lib/entity/data-structures/complex-key-map.ts
@@ -113,7 +113,7 @@ export class ComplexKeyMap<E extends Entity = Entity, V = unknown> {
         if (update && map.has(key)) {
             const updated = update(map.get(key) as V, value);
 
-            if (updated !== value) {
+            if (updated === value) {
                 return;
             }
 
