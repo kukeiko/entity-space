a4bb96f7f17a746328f1cd16e08985057264eec5
Magna Marek <magna.marek@gmail.com>
2022-10-07 22:42:23 +0200

    health: #30 simplified reduceQuery()

--- packages/core/src/lib/query/reduce-query.fn.ts
+++ packages/core/src/lib/query/reduce-query.fn.ts
@@ -2,32 +2,42 @@ import { Query } from "./query";
 
 // [todo] shouldn't be able to reduce queries w/ different entity-schemas
 export function reduceQuery(a: Query, b: Query): Query[] | false {
-    // [todo] not sure why i'm checking against undefined
-    const reducedCriteria =
-        b.getCriteria() === void 0
-            ? true
-            : a.getCriteria() === void 0
-            ? false
-            : b.getCriteria().reduce(a.getCriteria());
-
+    const reducedCriteria = b.getCriteria().reduce(a.getCriteria());
     const reducedExpansion = b.getExpansion().reduce(a.getExpansion());
 
     if (!reducedCriteria || !reducedExpansion) {
         return false;
-    } else if (reducedCriteria === true) {
-        if (reducedExpansion === true) {
-            return [];
-        }
-
-        const query = new Query({ entitySchema: a.getEntitySchema(), criteria: a.getCriteria(), expansion: reducedExpansion.getValue() });
+    } else if (reducedCriteria === true && reducedExpansion === true) {
+        return [];
+    } else if (reducedCriteria === true && reducedExpansion !== true) {
+        const query = new Query({
+            entitySchema: a.getEntitySchema(),
+            criteria: a.getCriteria(),
+            expansion: reducedExpansion.getValue(),
+        });
         return [query];
-    } else if (reducedExpansion === true) {
-        const query = new Query({ entitySchema: a.getEntitySchema(), criteria: reducedCriteria, expansion: a.getExpansionValue() });
+    } else if (reducedCriteria !== true && reducedExpansion === true) {
+        const query = new Query({
+            entitySchema: a.getEntitySchema(),
+            criteria: reducedCriteria,
+            expansion: a.getExpansionValue(),
+        });
         return [query];
-    } else {
+    } else if (reducedCriteria !== true && reducedExpansion !== true) {
         return [
-            new Query({ entitySchema: a.getEntitySchema(), criteria: reducedCriteria, expansion: a.getExpansionValue() }),
-            new Query({ entitySchema: a.getEntitySchema(), criteria: b.getCriteria(), expansion: reducedExpansion.getValue() }),
+            new Query({
+                entitySchema: a.getEntitySchema(),
+                criteria: reducedCriteria,
+                expansion: a.getExpansionValue(),
+            }),
+            new Query({
+                entitySchema: a.getEntitySchema(),
+                criteria: b.getCriteria(),
+                expansion: reducedExpansion.getValue(),
+            }),
         ];
+    } else {
+        // [todo] figure out why we have to provide this catch-all else case
+        throw new Error("can not happen. why does TypeScript complain? am I wrong?");
     }
 }
