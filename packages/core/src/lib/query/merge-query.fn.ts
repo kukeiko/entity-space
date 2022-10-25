d6674975e94d9213f1dbc786811cecad647b0122
Magna Marek <magna.marek@gmail.com>
2022-10-25 11:36:15 +0200

    health: #193 added more mergeQuery() tests & cleaned up the function a bit

--- packages/core/src/lib/query/merge-query.fn.ts
+++ packages/core/src/lib/query/merge-query.fn.ts
@@ -76,15 +76,6 @@ export function mergeQuery(a: Query, b: Query): false | Query {
             } else {
                 return false;
             }
-        } else {
-            // [todo] pretty sure this case can't happen?
-            return false;
-        }
-
-        if ((!pagingA && pagingB) || (pagingA && !pagingB)) {
-            return false;
-        } else if (!pagingA!.equivalent(pagingB!)) {
-            return false;
         }
     }
 
@@ -104,48 +95,18 @@ export function mergeQuery(a: Query, b: Query): false | Query {
     }
 
     const mergedCriteria = a.getCriteria().merge(b.getCriteria());
-    const expandReduced_A_by_B = b.getExpansion().reduce(a.getExpansion());
-    const expandReduced_B_by_A = a.getExpansion().reduce(b.getExpansion());
-
-    const aIsSubsetOfB = expandReduced_A_by_B !== false && Object.keys(expandReduced_A_by_B).length === 0;
-    const bIsSubsetOfA = expandReduced_B_by_A !== false && Object.keys(expandReduced_B_by_A).length === 0;
-
-    if (equivalentExpansion && mergedCriteria === false) {
-        return new Query({
-            entitySchema,
-            criteria: or(a.getCriteria(), b.getCriteria()),
-            expansion: a.getExpansionValue(),
-            options,
-        });
-    }
-
-    if (mergedCriteria === false) {
-        return false;
-    }
-
-    const criteria = mergedCriteria;
-    let expansion: ExpansionValue | undefined;
 
     if (equivalentExpansion) {
-        expansion = a.getExpansionValue();
-    } else if (aIsSubsetOfB) {
-        if (equivalentCriteria) {
-            expansion = b.getExpansionValue();
-        } else if (b.getCriteria().reduce(a.getCriteria()) === true) {
-            // [todo] redundant?
-            expansion = b.getExpansionValue();
+        if (mergedCriteria !== false) {
+            return new Query({ entitySchema, options, criteria: mergedCriteria, expansion: a.getExpansionValue() });
+        } else {
+            return new Query({
+                entitySchema,
+                criteria: or(a.getCriteria(), b.getCriteria()),
+                expansion: a.getExpansionValue(),
+                options,
+            });
         }
-    } else if (bIsSubsetOfA) {
-        if (equivalentCriteria) {
-            expansion = a.getExpansionValue();
-        } else if (a.getCriteria().reduce(b.getCriteria()) === true) {
-            // [todo] redundant?
-            expansion = a.getExpansionValue();
-        }
-    }
-
-    if (expansion) {
-        return new Query({ entitySchema, options, criteria, expansion });
     }
 
     return false;
--- packages/core/src/test/query/merge-query.test.ts
+++ packages/core/src/test/query/merge-query.test.ts
@@ -9,10 +9,25 @@ describe("mergeQuery()", () => {
     schemas.addSchema(songSchema);
     schemas.addSchema(fooSchema);
     schemas.addSchema(barSchema);
-
     fooSchema.addRelationProperty("bar", barSchema, "barId", "id");
 
     expectQuery("foo", schemas).plus("foo/{ bar }").toBe("foo/{ bar }");
+    expectQuery("foo", schemas).plus("song").toBe(false);
+    expectQuery(`foo<{ searchText: "bar" }>`, schemas).plus(`foo<{ searchText: "baz" }>`).toBe(false);
+
+    expectQuery("foo({ id: 7 })", schemas).plus("foo({ id: 3 })").toBe("foo({ id: { 7, 3 } })");
+    expectQuery("foo({ id: 7 })", schemas).plus("foo({ artistId: 13 })").toBe("foo({ id: 7 } | { artistId: 13 })");
+    expectQuery("foo({ id: 7 })/{ id, name }", schemas)
+        .plus("foo({ artistId: 13 })/{ id, name }")
+        .toBe("foo({ id: 7 } | { artistId: 13 })/{ id, name }");
+
+    expectQuery("foo({ id: 7 })/{ id, name }", schemas)
+        .plus("foo({ artistId: 13 })/{ id }")
+        .toBe(false);
+
+    expectQuery("foo({ id: 7 })/{ id, name }", schemas)
+        .plus("foo({ id: 3 })/{ id }")
+        .toBe(false);
 
     expectQuery("foo({ price: [100, 200], rating: [3, 8] })/{ foo }", schemas)
         .plus("foo({ price: [100, 200], rating: [3, 8] })/{ bar }")
@@ -22,23 +37,66 @@ describe("mergeQuery()", () => {
         .plus("foo({ price: [100, 200], rating: [3, 8] })/{ foo }")
         .toBe("foo({ price: [100, 200], rating: [3, 8] })/{ foo }");
 
-    expectQuery("song({ artistId: 7 })[0, 10]", schemas)
-        .plus("song({ artistId: 7 })[0, 10]")
-        .toBe("song({ artistId: 7 })[0, 10]");
+    describe("paging", () => {
+        {
+            // A & B equal
+            expectQuery("song({ artistId: 7 })[0, 10]", schemas)
+                .plus("song({ artistId: 7 })[0, 10]")
+                .toBe("song({ artistId: 7 })[0, 10]");
+        }
 
-    expectQuery("song({ artistId: 7 })[0, 10]", schemas)
-        .plus("song({ artistId: 7 })[0, 20]")
-        .toBe("song({ artistId: 7 })[0, 20]");
+        {
+            // A & B equivalent
+            expectQuery("song({ artistId: { 7 } })[0, 10]", schemas)
+                .plus("song({ artistId: 7 })[0, 10]")
+                .toBe("song({ artistId: { 7 } })[0, 10]");
+        }
 
-    expectQuery("song({ artistId: 7 })[0, 10]", schemas)
-        .plus("song({ artistId: 7 })[11, 20]")
-        .toBe("song({ artistId: 7 })[0, 20]");
+        {
+            // A has paging, B has not
+            expectQuery("song({ id: 7 })[0, 10]", schemas).plus("song({ id: 7 })").toBe("song({ id: 7 })");
+            expectQuery("song({ id: { 7 } })[0, 10]", schemas).plus("song({ id: 7 })").toBe("song({ id: 7 })");
+            expectQuery("song({ id: 7 })[0, 10]", schemas).plus("song({ id: 8 })").toBe(false);
+        }
 
-    expectQuery("song({ artistId: 7 })[0, 10]", schemas).plus("song({ artistId: 7 })[12, 20]").toBe(false);
+        {
+            // B has paging, A has not
+            expectQuery("song({ id: 7 })", schemas).plus("song({ id: 7 })[0, 10]").toBe("song({ id: 7 })");
+            expectQuery("song({ id: { 7 } })", schemas).plus("song({ id: 7 })[0, 10]").toBe("song({ id: { 7 } })");
 
-    expectQuery("song({ artistId: 7 })[0, 10]/{ id }", schemas)
-        .plus("song({ artistId: 7 })[0, 10]/{ name }")
-        .toBe("song({ artistId: 7 })[0, 10]/{ id, name }");
+            expectQuery("song({ id: 7 })/{ id }", schemas)
+                .plus("song({ id: 7 })[0, 10]/{ id }")
+                .toBe("song({ id: 7 })/{ id }");
 
-    expectQuery("song({ artistId: 7 })[0, 10]", schemas).plus("song({ artistId: { 7, 9 } })[0, 10]").toBe(false);
+            expectQuery("song({ id: 7 })", schemas).plus("song({ id: 8 })[0, 10]").toBe(false);
+        }
+
+        // A & B have paging
+        {
+            expectQuery("song({ id: 7 })[0, 10]", schemas)
+                .plus("song({ id: 7 })[0, 20]")
+                .toBe("song({ id: 7 })[0, 20]");
+
+            expectQuery("song({ id: 7 })[0, 10]", schemas)
+                .plus("song({ id: 7 })[11, 20]")
+                .toBe("song({ id: 7 })[0, 20]");
+
+            expectQuery("song({ artistId: 7 })[0, 10]", schemas).plus("song({ artistId: 7 })[12, 20]").toBe(false);
+            expectQuery("song({ artistId: 7 })[name, 0, 10]", schemas)
+                .plus("song({ artistId: 7 })[artist.name, 0, 10]")
+                .toBe(false);
+
+            expectQuery("song({ artistId: 7 })[0, 10]/{ id }", schemas)
+                .plus("song({ artistId: 7 })[0, 10]/{ name }")
+                .toBe("song({ artistId: 7 })[0, 10]/{ id, name }");
+
+            expectQuery("song({ artistId: 7 })[0, 10]/{ id }", schemas)
+                .plus("song({ artistId: 7 })[0, 11]/{ name }")
+                .toBe(false);
+
+            expectQuery("song({ artistId: 7 })[0, 10]", schemas)
+                .plus("song({ artistId: { 7, 9 } })[0, 10]")
+                .toBe(false);
+        }
+    });
 });
