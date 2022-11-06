288344c26d3657abf67a05eea1bf1f4088ab350c
Magna Marek <magna.marek@gmail.com>
2022-11-06 13:20:05 +0100

    health: #151 add small todo in token-type

--- packages/lexer/src/lib/token-type.enum.ts
+++ packages/lexer/src/lib/token-type.enum.ts
@@ -1,6 +1,7 @@
 export enum TokenType {
     Number = 0,
     String = 1,
+    // [todo] is there any reason to have this at all? why not just use "Special" instead?
     Combinator = 2,
     Literal = 3,
     Special = 4,
