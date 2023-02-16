0b7696a1a37bae0d6b8621979324295d4249171c
Magna Marek <magna.marek@gmail.com>
2023-02-16 21:53:09 +0100

    health: #202 remove criterion token parser tests - also from obsolete criterion implementation - there won't be a replacement as parse-criteria.test is good enough; all these tests really did is check that the lexer works

--- packages/core/src/test/parsing/any-parser.test.ts
+++
@@ -1,9 +0,0 @@
-import { any } from "../../lib/criteria/criterion/any/any.fn";
-import { anyCriterionTokenParser } from "../../lib/criteria/parser/any-criterion.token-parser";
-import { TokenType } from "../../lib/lexer/token-type.enum";
-import { token } from "../../lib/lexer/token.fn";
-import { itShouldParseTokens } from "./utils";
-
-describe("parse-tokens: any", () => {
-    itShouldParseTokens(anyCriterionTokenParser, [token(TokenType.Literal, "any")], any());
-});
