cd44ea23301009bf9b8c09042e568cdf3074fe3d
Magna Marek <magna.marek@gmail.com>
2024-01-27 14:58:03 +0100

    chore: #212 upgrade to typescript 5.3.3 & jest 29.7.0

--- packages/core/jest.config.ts
+++ packages/core/jest.config.ts
@@ -2,15 +2,15 @@
 export default {
     displayName: "core",
     preset: "../../jest.preset.js",
-    globals: {
-        "ts-jest": {
-            tsconfig: "<rootDir>/tsconfig.spec.json",
-        },
-    },
     transform: {
-        "^.+\\.[tj]s$": "ts-jest",
+        "^.+\\.[tj]s$": [
+            "ts-jest",
+            {
+                tsconfig: "<rootDir>/tsconfig.spec.json",
+            },
+        ],
     },
     moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
     coverageDirectory: "../../coverage/packages/core",
-    setupFiles: ["./jest-setup-file.ts"]
+    setupFiles: ["./jest-setup-file.ts"],
 };
