82e1ace8404349007e15e310decd2881b7820509
Magna Marek <magna.marek@gmail.com>
2025-12-02 21:31:21 +0100

    feat: #8 consider 0, "0" and "" to be temporary ids

--- src/elements/entity/entity-has-id.fn.ts
+++ src/elements/entity/entity-has-id.fn.ts
@@ -2,22 +2,24 @@ import { readPath } from "@entity-space/utils";
 import { Entity } from "./entity";
 import { EntitySchema } from "./entity-schema";
 
+function isValidIdValue(value: string | number | null | undefined): boolean {
+    return !(
+        value == null ||
+        (typeof value == "number" && value <= 0) ||
+        (typeof value == "string" && (value <= "0" || value === ""))
+    );
+}
+
 export function entityHasId(schema: EntitySchema, entity: Entity): boolean {
     if (!schema.hasId()) {
         return false;
     }
 
     for (const idPath of schema.getLeadingIdPaths()) {
-        if (readPath(idPath, entity) == null) {
+        if (!isValidIdValue(readPath(idPath, entity))) {
             return false;
         }
     }
 
-    const value = readPath<undefined | null | number | string>(schema.getLastIdPath(), entity);
-
-    if (value == null || (typeof value == "number" && value < 0) || (typeof value == "string" && value < "0")) {
-        return false;
-    }
-
-    return true;
+    return isValidIdValue(readPath(schema.getLastIdPath(), entity));
 }
